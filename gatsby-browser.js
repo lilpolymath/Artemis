2cb373e9d73443a9902a8c38945ff6085c3c331f
lilpolymath <ayofavour7@gmail.com>
2021-03-05 01:50:03 +0100

    feat: service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,17 @@
 // custom typefaces
-import "./src/style/all.scss"
-import "typeface-montserrat"
-import "typeface-merriweather"
-import "typeface-nunito"
-import "typeface-alegreya"
+import './src/style/all.scss';
+import 'typeface-montserrat';
+import 'typeface-merriweather';
+import 'typeface-nunito';
+import 'typeface-alegreya';
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  );
+
+  if (answer === true) {
+    window.location.reload();
+  }
+};
