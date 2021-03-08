490105ac991b910ff14032d0b4efa5bb52db3923
Favour <ayofavour7@gmail.com>
2021-03-08 15:00:17 +0100

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'Bolaji Olajide',
   shortName: 'Proton',
   description:
-    'I’m a full-stack software engineer based in Lagos, Nigeria. He currently builds exciting learning experiences with Pluralsight.',
+    'I’m a full-stack software engineer based in Lagos, Nigeria and I’m currently building exciting learning experiences with Pluralsight.',
   url: 'https://bolaji.de', // Domain of your site without prefix!
   prefix: '/',
   author: 'bolaji',
